16f932bd715322712fb2a6d1135619c0f8cb65c5
Vladyslav Kubyshkin <v.kubyshkin2001@hotmail.com>
2022-02-07 00:38:33 +0100

    Lesson 46

---
+++ DesktopContactsApp/DesktopContactsApp/Classes/Contact.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesktopContactsApp.Classes
+{
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Email: {Email}, Phone: {Phone}";
+        }
+    }
+}
---
+++ DesktopContactsApp/DesktopContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ReadDatabase();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            newContactWindow.ShowDialog();
+            ReadDatabase();
+        }
+
+        void ReadDatabase()
+        {
+            List<Contact> contacts;
+            using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contact>();
+                contacts = conn.Table<Contact>().ToList();
+            }
+
+            if (contacts != null)
+            {
+                //foreach (var item in contacts)
+                //{
+                //    contactsListView.Items.Add(new ListViewItem()
+                //    { 
+                //        Content = item
+                //    });
+                //}
+
+                contactsListView.ItemsSource = contacts;
+            }
+        }
+
+        public void ClearButton_Click()
+        {
+
+        }
+    }
+}
