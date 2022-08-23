b49dc69ab1565c5fa1ebfa715b62a4ca3f67c11c
Vladyslav Kubyshkin <v.kubyshkin2001@hotmail.com>
2022-08-23 16:40:13 +0200

    Speech added

--- MyNote/MyNote/View/NotesWindow.xaml.cs
+++ MyNote/MyNote/View/NotesWindow.xaml.cs
@@ -1,13 +1,39 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace MyNote.View;
 
 public partial class NoteWindow : Window
 {
+    private SpeechRecognitionEngine _recognizer;
     public NoteWindow()
     {
         InitializeComponent();
+
+        var currentCulture = (from r in SpeechRecognitionEngine.InstalledRecognizers()
+            where r.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+            select r).FirstOrDefault();
+        _recognizer = new SpeechRecognitionEngine();
+
+        GrammarBuilder builder = new GrammarBuilder();
+        builder.AppendDictation();
+        Grammar grammar = new Grammar(builder);
+
+        _recognizer.LoadGrammar(grammar);
+        _recognizer.SetInputToDefaultAudioDevice();
+        _recognizer.SpeechRecognized += RecognizerEvent;
+    }
+
+    private void RecognizerEvent(object? sender, SpeechRecognizedEventArgs e)
+    {
+        string recognizedText = e.Result.Text;
+        ContentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recognizedText)));
     }
 
     private void MenuItem_OnClick(object sender, RoutedEventArgs e)
@@ -15,13 +41,35 @@ public partial class NoteWindow : Window
         Application.Current.Shutdown();
     }
 
+    private bool isRecognizing = false;
     private void SpeechButton_Click(object sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        if (!isRecognizing)
+        {
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            isRecognizing = true;
+        }
+        else
+        {
+            _recognizer.RecognizeAsyncStop();
+            isRecognizing = false;
+        }
     }
 
     private void ContentRichTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        int amountOfCharacters = (new TextRange(
+            ContentRichTextBox.Document.ContentStart,
+            ContentRichTextBox.Document.ContentEnd)
+            .Text.Length);
+
+        StatusTextBlock.Text = $"Document length: {amountOfCharacters}";
+    }
+
+    private void BoldButton_Click(object sender, RoutedEventArgs e)
+    {
+        var textToBold = new TextRange(ContentRichTextBox.Selection.Start, ContentRichTextBox.Selection.End);
+        
+        ContentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
     }
 }
